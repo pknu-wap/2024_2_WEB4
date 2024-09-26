97fa3eb3918a9cb032c916117e58f38c2ef66913
YSPark <parkite576@gmail.com>
2024-09-26 11:59:01 +0900

    BE/feat/FE에서 BE로 이미지 전송 완료, 전송시 이름 해시값으로 설정해서 중복 삭제

--- server/main.py
+++ server/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+import hashlib
+import os
+from fastapi import FastAPI, File, UploadFile
+from typing import List
 
 app = FastAPI()
 
+UPLOAD_FOLDER = "./uploads/"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/ganz/upload/")
+async def upload_images(files: List[UploadFile] = File(...)):
+    if len(files) != 2:
+        return {"error": "You must upload exactly two images."}
+    
+    file_paths = []
+
+    for file in files:
+        file_contents = await file.read()  
+        file_hash = hashlib.md5(file_contents).hexdigest() # 해시 값 사용 
+        file_extension = os.path.splitext(file.filename)[1] # 확장자 
+        unique_filename = f"{file_hash}{file_extension}"  
+        # 파일 경로 생성 및 저장
+        file_location = os.path.join(UPLOAD_FOLDER, unique_filename)  
+        with open(file_location, "wb") as buffer:
+            buffer.write(file_contents)  
+        
+        file_paths.append(file_location)
+
+    return {"message": "Files successfully uploaded", "file_paths": file_paths}
