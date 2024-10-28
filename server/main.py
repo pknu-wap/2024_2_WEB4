020d41284a172f9b8021f9dbbce11574eb217f7e
YSPark <parkite576@gmail.com>
2024-10-28 11:33:51 +0900

    Feat / uuid로 폴더 생성

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
-import hashlib
-import os
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, Cookie, File, UploadFile, Response
 from fastapi.middleware.cors import CORSMiddleware
+
 from typing import List
+import os
+import uuid
 
 app = FastAPI()
 
@@ -18,21 +19,27 @@ app.add_middleware(
 UPLOAD_FOLDER = "./uploads/"
 
 @app.post("/ganz/upload/")
-async def upload_images(files: List[UploadFile] = File(...)):
+async def upload_images(response: Response, files: List[UploadFile] = File(...), session_id: str = Cookie(None)):
+    # 세션 ID가 없으면 생성하여 쿠키에 저장
+    if not session_id:
+        session_id = str(uuid.uuid4())
+        response.set_cookie(key="session_id", value=session_id)
+
     if len(files) != 2:
         return {"error": "You must upload exactly two images."}
     
+    # 사용자별 디렉터리 생성
+    user_folder = os.path.join(UPLOAD_FOLDER, session_id)
+    os.makedirs(user_folder, exist_ok=True)
+
     file_paths = []
 
     for file in files:
-        file_contents = await file.read()  
-        file_hash = hashlib.md5(file_contents).hexdigest() # 해시 값 사용 
-        file_extension = os.path.splitext(file.filename)[1] # 확장자 
-        unique_filename = f"{file_hash}{file_extension}"  
-        # 파일 경로 생성 및 저장
-        file_location = os.path.join(UPLOAD_FOLDER, unique_filename)  
+        file_contents = await file.read()
+        unique_filename = f"{uuid.uuid4()}{os.path.splitext(file.filename)[1]}"
+        file_location = os.path.join(user_folder, unique_filename)
         with open(file_location, "wb") as buffer:
-            buffer.write(file_contents)  
+            buffer.write(file_contents)
         
         file_paths.append(file_location)
 
